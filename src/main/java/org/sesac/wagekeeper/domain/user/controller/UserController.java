4a992d00c9a77d41fed942b71df3beb3ca1dc271
hysong4u <hayeon_song@naver.com>
2024-07-30 17:37:46 +0900

    [feat] #1 controller 구현

---
+++ src/main/java/org/sesac/wagekeeper/domain/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package org.sesac.wagekeeper.domain.user.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+@RestController
+public class UserController {
+}
