8d0e1bd42cd88b0f5f2c6504e13764ce72e3f3c9
hysong4u <hayeon_song@naver.com>
2024-07-30 17:40:43 +0900

    [feat] #1 service 구현

---
+++ src/main/java/org/sesac/wagekeeper/domain/workinglog/service/WorkingLogService.java
@@ -0,0 +1,11 @@
+package org.sesac.wagekeeper.domain.workinglog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class WorkingLogService {
+}
