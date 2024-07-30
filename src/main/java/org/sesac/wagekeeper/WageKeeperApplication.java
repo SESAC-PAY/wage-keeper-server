0cc1b21143130f76fb80dedf8d934ce708586251
hysong4u <hayeon_song@naver.com>
2024-07-30 14:56:05 +0900

    [chore] WageKeeperApplication 생성

---
+++ src/main/java/org/sesac/wagekeeper/WageKeeperApplication.java
@@ -0,0 +1,13 @@
+package org.sesac.wagekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WageKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sesac/wagekeeper/WageKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sesac.wagekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
